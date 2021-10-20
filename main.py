accb60f937d21a46fda038268257f7218f64b3e5
saianirudh1213 <64244473+saianirudh1213@users.noreply.github.com>
2021-10-20 18:42:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+arr=[83,1,45,95,45,52,11,47,0,45,67,82]
+arr.sort()
+print(arr)
